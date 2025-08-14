bf64753e9703934ee828a21de4ed14f8f1e6d1cf
Manas Ranjan Bera <manasbera484@gmail.com>
2025-08-14 19:56:05 +0530

    init

---
+++ JavaScript/Class 1/script.js
@@ -0,0 +1,21 @@
+// console.log("Hello everyone");
+
+//! Variable Keyword
+//? var
+var a = 10
+// console.log(a);
+var a = "Hello"
+// console.log(a);
+a = true
+// console.log(a);
+
+//? let
+let b
+// let b //not allowed
+b = "Hi"
+console.log(b);
+
+//? const
+const c = true
+// const c //not allowed
+// c = 1 //not allowed
