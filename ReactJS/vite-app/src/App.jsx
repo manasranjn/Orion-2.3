ed56e4bd765a6e99a2fc97210ac139496799253e
Manas Ranjan Bera <manasbera484@gmail.com>
2025-09-09 18:17:16 +0530

    React setup

---
+++ ReactJS/vite-app/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Hello</h1>
+      <div>
+        <p>This is a paragraph</p>
+      </div>
+
+      <p>This is a paragraph</p>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default App;
