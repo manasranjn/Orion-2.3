e1a0257122096c81ff14899424dadf80001e58f2
Manas Ranjan Bera <manasbera484@gmail.com>
2025-10-16 18:32:46 +0530

    Node module system

---
+++ Backend/Node/Modules/CommonJS/app.js
@@ -0,0 +1,18 @@
+console.log("Hello everyone, this is App.js");
+
+//! Import addition function from math module
+// const add = require("./math")
+
+// add(20, 10)
+
+//! Import the entire math module
+// const math = require("./math")
+
+// math.addition(200, 50)
+// math.subtraction(500, 90)
+
+//! Import the entire math module by destructuring
+const { addition, subtraction } = require("./math")
+
+addition(200, 50)
+subtraction(500, 90)
