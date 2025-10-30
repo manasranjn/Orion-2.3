b492b8e0aacbff661c92944c3e34c4d03885cf6f
Manas Ranjan Bera <manasbera484@gmail.com>
2025-10-30 18:05:20 +0530

    Routing

--- Backend/Express/Basic-Server/index.js
+++ Backend/Express/Basic-Server/index.js
@@ -1,10 +1,38 @@
-const express = require('express')
-const app = express()
+const express = require("express");
 
-app.get('/', (req, res) => {
-    res.send("Hello, this is my express server")
-})
+//Initialize express
+const app = express();
 
-app.listen(5000, () => {
-    console.log(`Server running on http://localhost:${5000}`);
-})
+//Basic Route
+app.get("/", (req, res) => {
+    res.send("Hello from Express");
+});
+
+//About Route
+app.get("/about", (req, res) => {
+    res.send("About us page");
+});
+
+//GET - Get data
+//POST - Send data
+//PUT/PATCH - update data
+//DELETE - delete
+
+//Contact Route
+app.get("/contact", function (req, res) {
+    res.send("Contact us page");
+});
+
+//Signup route
+app.post("/register", (req, res) => {
+    res.send("Registerd");
+});
+
+app.get("/register", (req, res) => {
+    res.send("Register route");
+});
+
+//Start the server
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
---
+++ Backend/Express/mini-store/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+
+const app = express()
+
+//Home
+app.get('/', (req, res) => {
+    res.send("Welcome to our Store")
+});
+
+//Products
+app.get('/products', (req, res) => {
+    res.send("This is products page")
+});
+
+//About Us
+app.get('/about', (req, res) => {
+    res.send("This is about us page")
+});
+
+//Contact us
+app.get('/contact', (req, res) => {
+    res.send("This is contact us page")
+});
+
+// Route parameters
+app.get("/products/:id", (req, res) => {
+    console.log(req.params.id);
+    res.send(`You requested product with ID = ${req.params.id}`);
+});
+
+
+app.listen(8000, () => {
+    console.log('Server running on http://localhost:8000');
+
+})
