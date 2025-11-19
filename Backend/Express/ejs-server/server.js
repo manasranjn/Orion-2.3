70e7e55d732db5624a056dae800940532ac856d9
Manas Ranjan Bera <manasbera484@gmail.com>
2025-11-19 17:49:11 +0530

    init

---
+++ Backend/Express/ejs-server/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 5000;
+
+//set EJS as the view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+
+app.get("/", (req, res) => {
+    res.render("index", {
+        title: "Home Page",
+    });
+});
+
+app.get("/about", (req, res) => {
+    res.render("about", {
+        title: "About Page",
+    });
+});
+
+app.get("/variables", (req, res) => {
+    res.render("variables", {
+        title: "EJS Variables",
+        user: {
+            name: "ABCD",
+            age: 25,
+            email: "abcd@gmail.com",
+            isActive: true,
+        },
+    });
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact");
+});
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
